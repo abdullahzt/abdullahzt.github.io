f9ba9d58805d6364c60e7681eed6dbaddf94f5f1
Abdullah Zubair Tanwir <abdullahzubair075@gmail.com>
2020-09-18 12:56:45 +0500

    Updates

---
+++ precache-manifest.2049cc69159152026c8df1df049a44a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21eb773037d7266703011179fe6a227",
+    "url": "/index.html"
+  },
+  {
+    "revision": "950b5f89d9f8b52af642",
+    "url": "/static/css/main.cb13d991.chunk.css"
+  },
+  {
+    "revision": "cd97206575a92692694d",
+    "url": "/static/js/2.dcad3392.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dcad3392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "950b5f89d9f8b52af642",
+    "url": "/static/js/main.888cdab0.chunk.js"
+  },
+  {
+    "revision": "35ec47dfee4c65c275be",
+    "url": "/static/js/runtime-main.c6e37e88.js"
+  },
+  {
+    "revision": "424353db81966077480622d85e39715c",
+    "url": "/static/media/me.424353db.jpeg"
+  }
+]);
