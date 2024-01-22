ed0a27da34598c7373703bd1cb6240e49f83f741
Abdullah Zubair Tanwir <abdullahzubair075@gmail.com>
2024-01-22 19:19:22 +0500

    Updated contact

--- src/components/UI/ContactButton/ContactButton.tsx
+++ src/components/UI/ContactButton/ContactButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './ContactButton.module.css'
 
 const ContactButton: React.FC = props => (
-    <a href="mailTo:abdullahzubair075@gmail.com" className={classes.ContactButton} >
+    <a href="mailTo:abdullahzubair99@gmail.com" className={classes.ContactButton} >
         {props.children}
     </a>
 )
