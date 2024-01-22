60f023ce25253b7b4f51708f18b23360e5ba7d7f
Abdullah Zubair Tanwir <abdullahzubair075@gmail.com>
2024-01-22 19:17:30 +0500

    Updates

--- precache-manifest.f08fbce1c658884ee7c04f80175ab7e7.js
+++ precache-manifest.f08fbce1c658884ee7c04f80175ab7e7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d21eb773037d7266703011179fe6a227",
+    "revision": "bac3159933cf432da799381b0f90ce80",
     "url": "/index.html"
   },
   {
-    "revision": "950b5f89d9f8b52af642",
+    "revision": "549cf80b75b9519c8993",
     "url": "/static/css/main.cb13d991.chunk.css"
   },
   {
-    "revision": "cd97206575a92692694d",
-    "url": "/static/js/2.dcad3392.chunk.js"
+    "revision": "fd7f5ebfc5b29dabd6c5",
+    "url": "/static/js/2.1f095175.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.dcad3392.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.1f095175.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "950b5f89d9f8b52af642",
-    "url": "/static/js/main.888cdab0.chunk.js"
+    "revision": "549cf80b75b9519c8993",
+    "url": "/static/js/main.36b44343.chunk.js"
   },
   {
     "revision": "35ec47dfee4c65c275be",
